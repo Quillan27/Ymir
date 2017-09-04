ec9ca481c5056d37224067f021b19e2fa033800b
kramberg27 <bloNdie772>
2017-09-04 13:38:17 -0500

    Images can now be displayed, cleaner sidebar.

--- src/Map.java
+++ src/Map.java
@@ -1,13 +1,66 @@
 //Karl Ramberg
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Map {
+public class Map extends JLabel {
 
-    public int width;
-    public int height;
+    private int width;
+    private int height;
+    public BufferedImage mapImg;
+    private Dimension d;
 
-    public Map(int width,  int height){
+    private int color;
+
+    public Map(int width, int height) {
+
+        this.width = width;
+        this.height = height;
+
+        d = new Dimension(width, height);
+        setPreferredSize(d);
+
+        //default to black
+        color = toRGBAInt(119,148,198,255);
+
+        mapImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        newMap(getRandomWorldName());
+
+    }
+
+    public void newMap(String name) {
+
+        //map changes, solid for now
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                mapImg.setRGB(i, j, color);
+                System.out.println(color);
+            }
+        }
+
+        //display map
+        setIcon(new ImageIcon(mapImg));
+
+    }
+
+    //TODO finish method
+    public String getRandomWorldName(){
+
+        return "New World";
+
+    }
+
+    private int toRGBAInt(int r, int g, int b, int a){
+
+        int c = 0;
+        c += a<<24;
+        c += r<<16;
+        c += g<<8;
+        c += b;
+        System.out.println("Color: "+c);
+        return c;
 
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -2,35 +2,52 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ImageObserver;
+import java.awt.image.RenderedImage;
+import java.awt.image.renderable.RenderableImage;
+import java.nio.Buffer;
+import java.text.AttributedCharacterIterator;
 
 //The builder class for the applications window.
 public class Window extends JFrame {
 
     //TODO Change dynamically based on OS.
-    public final String LOOKANDFEEL = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+    private final String LOOKANDFEEL = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
 
     //Panel to the right with controls.
-    //public Sidebar s;
+    private Sidebar sidebar;
+    private Map map;
+    private Container container;
 
-    public Container c;
+    public Window(String title, int mapWidth, int mapHeight, Sidebar sidebar, Map map){
 
-    public Window(String title, int mapWidth, int mapHeight, Sidebar s){
+        this.sidebar = sidebar;
+        this.map = map;
 
         //Window Settings
         setTitle(title);
         setLookAndFeel();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(mapWidth + 200, mapHeight);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(mapWidth + 200, mapHeight); //200 extra for the sidebar
         setLocationRelativeTo(null);
         setResizable(false);
 
-        //Add Sidebar aligned to the left.
-        c = getContentPane();
-        c.add(s, BorderLayout.EAST);
+        //Add Sidebar aligned to the left and Map to the right.
+        container = getContentPane();
+        container.add(sidebar, BorderLayout.EAST);
+        container.add(map, BorderLayout.WEST);
 
     }
 
+
+
     public void setLookAndFeel(){
+
         try {
             UIManager.setLookAndFeel(LOOKANDFEEL);
         } catch (IllegalAccessException e) {
@@ -42,5 +59,7 @@ public class Window extends JFrame {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
+
 }
