2f3f94be1b523afa1fecfc18a367070c915c8e2a
karleramberg <kramberg27@gmail.com>
2019-01-02 20:35:29 -0600

    backup

--- src/main/java/com/karl/ymir/World.java
+++ src/main/java/com/karl/ymir/World.java
@@ -19,26 +19,26 @@ public class World {
         elevationGrid = new double[width][height];
 
         initElevation();
-        addPeaks();
+        addPeaks(20);
         addNoise();
     }
 
-    private void initElevation() {
-        for(int x = 0; x < width; x++) { // start flat in the middle
+    private void initElevation() { // the earth is flat
+        for(int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 elevationGrid[x][y] = 0.0;
             }
         }
     }
 
-    private void addPeaks() {
+    private void addPeaks(int count) { //TODO so many magic numbers, i might actually be merlin
         Random r = new Random(System.currentTimeMillis());
-        int[][] peaks = new int[100][2];
+        int[][] peaks = new int[count][2];
 
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < count; i++) {
             int randX = r.nextInt(width);
             int randY =  r.nextInt(height);
-            System.out.println(randX + " " + randY);
+            //System.out.println(randX + " " + randY);
             elevationGrid[randX][randY] = 0.75 + (1.0 - 0.75) * r.nextDouble();
             peaks[i][0] = randX;
             peaks[i][1] = randY;
@@ -47,19 +47,19 @@ public class World {
         for(int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 double elevScore = 0.0;
-                for(int i = 0; i < 20; i++) {
+                for(int i = 0; i < count; i++) {
                     int dis = getDis(x, y, peaks[i][0], peaks[i][1]);
                     if(dis < 350) {
                         elevScore += 350 - dis;
                     }
                 }
-                elevationGrid[x][y] += elevScore / 1250;
+                elevationGrid[x][y] += elevScore / 1200;
                 elevationGrid[x][y] = chomp(elevationGrid[x][y], 0.0, 1.0);
             }
         }
     }
 
-    private void addNoise() {
+    private void addNoise() { // shitty random noise with no pattern, perlin would spit on this
         Random r = new Random(System.currentTimeMillis());
         for(int x = 0; x < width; x++) {
             for(int y = 0; y < height; y++) {
