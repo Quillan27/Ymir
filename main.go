c4be46c0892c4d0a34ffd9e7ac7437bccdc67dc2
karledramberg <kramberg27@gmail.com>
2019-03-22 21:52:39 -0500

    basic world creation / manipulation

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"math/rand"
 )
 
+type World struct {
+    Grid [][][]float32
+    Name string
+}
+   
+func newWorld() (w *World) {
+    w = &World{}
+    
+    w.Grid = make([][][]float32, 4)
+    for i := range w.Grid {
+	w.Grid[i] = make([][]float32, 10)
+	for j := range w.Grid[i] {
+	    w.Grid[i][j] = make([]float32, 10)
+	}
+    }
+    
+    genWorld(w)
+
+    w.Name = newWorldName()
+
+    return w
+}
+
+func genWorld(w *World) {
+    for i := range w.Grid {
+	for j := range w.Grid[i] {
+	    for k := range w.Grid[i][j] {
+		w.Grid[i][j][k] += float32(rand.Intn(256))
+	    }
+	}
+    }
+}
+
+func newWorldName() string {
+    return "New World"
+}
+
 func main() {
-    fmt.Printf("Hello, Ymir")
+    fmt.Printf("Welcome to Ymir!\n")
+    w := *newWorld()	
+    for i := range w.Grid {
+	for j := range w.Grid[i] {
+	    fmt.Println(w.Grid[i][j])
+	}
+	fmt.Printf("\n")
+    }
 }
