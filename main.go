daea6ff11ad25f8d319fc70a026fd9d4fcf45bc1
karledramberg <kramberg27@gmail.com>
2019-03-22 22:28:14 -0500

    better printing

--- main.go
+++ main.go
@@ -5,25 +5,28 @@ import (
 	"math/rand"
 )
 
+const ELEVATION int = 0
+const CLIMATE int = 1
+const POLITICAL int = 2
+const BIOME int = 3
+
 type World struct {
-    Grid [][][]float32
+    Grid [][][]float64
     Name string
 }
-   
-func newWorld() (w *World) {
-    w = &World{}
+  
+func newWorld(width, height int) (*World) {
+    w := new(World)
     
-    w.Grid = make([][][]float32, 4)
+    w.Grid = make([][][]float64, 4)
     for i := range w.Grid {
-	w.Grid[i] = make([][]float32, 10)
+	w.Grid[i] = make([][]float64, width)
 	for j := range w.Grid[i] {
-	    w.Grid[i][j] = make([]float32, 10)
+	    w.Grid[i][j] = make([]float64, height)
 	}
     }
-    
     genWorld(w)
-
-    w.Name = newWorldName()
+    nameWorld(w)
 
     return w
 }
@@ -32,22 +35,36 @@ func genWorld(w *World) {
     for i := range w.Grid {
 	for j := range w.Grid[i] {
 	    for k := range w.Grid[i][j] {
-		w.Grid[i][j][k] += float32(rand.Intn(256))
+		w.Grid[i][j][k] += float64(rand.Float64())
 	    }
 	}
     }
 }
 
-func newWorldName() string {
-    return "New World"
+func nameWorld(w *World) {
+    w.Name = "New World"
 }
 
 func main() {
     fmt.Printf("Welcome to Ymir!\n")
-    w := *newWorld()	
+    w := *newWorld(10, 10)	
     for i := range w.Grid {
+	if i == ELEVATION {
+	    fmt.Print("Elevation\n")
+	} else if i == CLIMATE {
+	    fmt.Print("Climate\n")
+	} else if i == POLITICAL {
+	    fmt.Print("Political\n")
+	} else {
+	    fmt.Print("Biome\n")
+	}
+
 	for j := range w.Grid[i] {
-	    fmt.Println(w.Grid[i][j])
+	    for k := range w.Grid[i][j] {
+		fmt.Printf("%02d", int(w.Grid[i][j][k]*31.0))
+		fmt.Printf(" ")
+	    }
+	    fmt.Printf("\n")
 	}
 	fmt.Printf("\n")
     }
