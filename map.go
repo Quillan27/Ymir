51772174f3e70a211d24859fd11d22440e712c87
karledramberg <kramberg27@gmail.com>
2019-03-26 22:22:35 -0500

    more cleanup, fixed tabs

--- map.go
+++ map.go
@@ -1,88 +1,85 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"image"
 	"image/color"
 	"image/png"
+	"bufio"
 	"os"
 	"strconv"
 )
 
-// redraws map based on the world grid
-func (w *World) updateMap(view int) {
-	// get color palette for map view
-	p, hexes := createPalette(view)
+func (w *World) drawMap(view int) {
+    fmt.Printf("Drawing the map...\n")
 
-	// make new image based on grid
-	w.Map = *image.NewRGBA(image.Rect(0, 0, len(w.Grid[view]), len(w.Grid[view][0])))
-	for y := 0; y < w.Map.Bounds().Max.X; y++ {
-		for x := 0; x < w.Map.Bounds().Max.Y; x++ {
-			c := p[int(w.Grid[x][y][view]*32.0)]
-			w.Map.Set(x, y, c)
-		}
+    // get color palette for map view
+    p := createPalette(view)
+
+    // make new image based on grid
+    w.Map = *image.NewRGBA(image.Rect(0, 0, len(w.Grid[view]), len(w.Grid[view][0])))
+    for y := 0; y < w.Map.Bounds().Max.X; y++ {
+	for x := 0; x < w.Map.Bounds().Max.Y; x++ {
+	    c := p[int(w.Grid[x][y][view]*32.0)]
+	    w.Map.Set(x, y, c)
 	}
+    }
 }
 
-// saves a map.png to the disk
+func createPalette(view int) (p color.Palette) {
+    var path string
+    if view == ELEVATION {
+	path = "res/palettes/elevation.pal"
+    } else if view == CLIMATE {
+	path = "res/palettes/climate.pal"
+    } else if view == POLITICAL {
+	path = "res/palettes/political.pal"
+    } else {
+	path = "res/palettes/biome.pal"
+    }
+
+    hexColors, err := splitLines(path)
+    if err != nil {
+	fmt.Print(path + "could not be opened.\n")
+    }
+
+    for i := 0; i < len(hexColors); i++ {
+	r, _ := strconv.ParseUint(hexColors[i][1:3], 16, 8)
+	g, _ := strconv.ParseUint(hexColors[i][3:5], 16, 8)
+	b, _ := strconv.ParseUint(hexColors[i][5:7], 16, 8)
+	var a uint8 = 255
+	c := color.RGBA{uint8(r), uint8(g), uint8(b), a}
+	p = append(p, c)
+    }
+
+    return    
+}
+
+func splitLines(path string) (lines []string, scanErr error) {
+    file, err := os.Open(path)
+    if err != nil {
+	return nil, err
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+	lines = append(lines, scanner.Text())
+    }
+    scanErr = scanner.Err()
+
+    return
+}
+
 func (w *World) saveMap() {
-	// create a new file
-	f, err := os.Create("map.png")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+    fmt.Printf("Saving the map to disk...\n")
 
-	// write in png format
-	png.Encode(f, &w.Map)
+    f, err := os.Create("map.png")
+    if err != nil {
+	panic(err)
+    }
+    defer f.Close()
+
+    png.Encode(f, &w.Map)
 }
 
-// makes a array of colors from a text file
-func createPalette(view int) (color.Palette, []string) {
-	// get colors from a file in hexidecimal format
-	var path string
-	if view == ELEVATION {
-		path = "res/palettes/elevation.pal"
-	} else if view == CLIMATE {
-		path = "res/palettes/climate.pal"
-	} else if view == POLITICAL {
-		path = "res/palettes/political.pal"
-	} else {
-		path = "res/palettes/biome.pal"
-	}
-	
-	hexColors, err := readLines(path)
-	if err != nil {
-		fmt.Print(path + "could not be opened.\n")
-	}
-
-	// convert hexidecimal colors to RGBA and add to a palette
-	p := color.Palette{}
-	for i := 0; i < len(hexColors); i++ {
-		r, _ := strconv.ParseUint(hexColors[i][1:3], 16, 8)
-		g, _ := strconv.ParseUint(hexColors[i][3:5], 16, 8)
-		b, _ := strconv.ParseUint(hexColors[i][5:7], 16, 8)
-		var a uint8 = 255
-		c := color.RGBA{uint8(r), uint8(g), uint8(b), a}
-		p = append(p, c)
-	}
-
-	return p, hexColors
-}
-
-// opens a file and splits it's line into an array
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
--- world.go
+++ world.go
@@ -1,43 +1,50 @@
 package main
 
 import (
-	"image"
-	"math/rand"
-	"time"
+    "fmt"
+    "image"
+    "math/rand"
+    "time"
 )
 
 type World struct {
-	Grid [][][]float64
-	Map  image.RGBA
-	Name string
+    Grid [][][]float64
+    Map  image.RGBA
+    Name string
 }
 
 func newWorld(width, height int) (w *World) {
-	w = new(World)
-	w.Grid = make([][][]float64, width)
-	for i := range w.Grid {
-		w.Grid[i] = make([][]float64, height)
-		for j := range w.Grid[i] {
-			w.Grid[i][j] = make([]float64, 4)
-		}
+    fmt.Printf("Creating a new world...\n")
+ 
+    w.Grid = make([][][]float64, width)
+    for i := range w.Grid {
+	w.Grid[i] = make([][]float64, height)
+	for j := range w.Grid[i] {
+	    w.Grid[i][j] = make([]float64, 4)
 	}
-	w.generate()
-	w.updateMap(ELEVATION)
-	w.updateName()
-	return
+    }
+    w.generate()
+    w.drawMap(ELEVATION)
+    w.name()
+
+    return
 }
 
 func (w *World) generate() {
-	rand.Seed(time.Now().UnixNano())
-	for i := range w.Grid {
-		for j := range w.Grid[i] {
-			for k := range w.Grid[i][j] {
-				w.Grid[i][j][k] += rand.Float64()
-			}
-		}
+    fmt.Printf("Adding world features...\n")
+
+    rand.Seed(time.Now().UnixNano())
+    for i := range w.Grid {
+	for j := range w.Grid[i] {
+	    for k := range w.Grid[i][j] {
+		w.Grid[i][j][k] += rand.Float64()
+	    }
 	}
+    }
 }
 
-func (w *World) updateName() {
-	w.Name = "New World"
+func (w *World) name() {
+    fmt.Printf("Naming the world...\n")
+    
+    w.Name = "New World"
 }
