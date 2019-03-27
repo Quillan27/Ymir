6d72e68556ee89ee9c8231e68d027afd454ce9a6
karledramberg <kramberg27@gmail.com>
2019-03-26 20:14:54 -0500

    clean up debugging, add comments

--- main.go
+++ main.go
@@ -10,31 +10,8 @@ const POLITICAL int = 2
 const BIOME int = 3
 
 func main() {
-    fmt.Printf("Welcome to Ymir!\n\n")
+	fmt.Printf("Welcome to Ymir!\n\n")
 
-    w := newWorld(10, 10)	
-    
-    fmt.Print(w.Name+"\n")
-    fmt.Print("-----------\n")
-    for i := range w.Grid {
-	if i == ELEVATION {
-	    fmt.Print("Elevation\n")
-	} else if i == CLIMATE {
-	    fmt.Print("Climate\n")
-	} else if i == POLITICAL {
-	    fmt.Print("Political\n")
-	} else {
-	    fmt.Print("Biome\n")
-	}
-
-	for j := range w.Grid[i] {
-	    for k := range w.Grid[i][j] {
-		fmt.Printf("%02d", int(w.Grid[i][j][k] * 31.0))
-		fmt.Printf(" ")
-	    }
-	    fmt.Printf("\n")
-	}
-	fmt.Printf("\n")
-    }
-    w.saveMap()
+	w := newWorld(400, 200)
+	w.saveMap()
 }
--- world.go
+++ world.go
@@ -1,41 +1,43 @@
-package main 
+package main
 
 import (
-    "math/rand"
-    "image"
+	"image"
+	"math/rand"
+	"time"
 )
 
 type World struct {
-    Grid [][][]float64
-    Map image.RGBA
-    Name string
+	Grid [][][]float64
+	Map  image.RGBA
+	Name string
 }
 
 func newWorld(width, height int) (w *World) {
-    w = new(World)
-    w.Grid = make([][][]float64, 4)
-    for i := range w.Grid {
-	w.Grid[i] = make([][]float64, width)
-	for j := range w.Grid[i] {
-	    w.Grid[i][j] = make([]float64, height)
+	w = new(World)
+	w.Grid = make([][][]float64, width)
+	for i := range w.Grid {
+		w.Grid[i] = make([][]float64, height)
+		for j := range w.Grid[i] {
+			w.Grid[i][j] = make([]float64, 4)
+		}
 	}
-    }
-    w.generate()
-    w.updateMap(ELEVATION)
-    w.updateName()
-    return 
+	w.generate()
+	w.updateMap(ELEVATION)
+	w.updateName()
+	return
 }
 
 func (w *World) generate() {
-    for i := range w.Grid {
-	for j := range w.Grid[i] {
-	    for k := range w.Grid[i][j] {
-		w.Grid[i][j][k] += rand.Float64()
-	    }
+	rand.Seed(time.Now().UnixNano())
+	for i := range w.Grid {
+		for j := range w.Grid[i] {
+			for k := range w.Grid[i][j] {
+				w.Grid[i][j][k] += rand.Float64()
+			}
+		}
 	}
-    }
 }
 
 func (w *World) updateName() {
-    w.Name = "New World"
+	w.Name = "New World"
 }
